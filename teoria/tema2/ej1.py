f647fbe1d7a721daff22bcc9ba3b154c80a05ff5
mgmacias95 <mgmacias95@correo.ugr.es>
2017-03-01 22:58:06 +0100

    queda pasar dataframe a markdown

--- teoria/tema2/ej1.py
+++ teoria/tema2/ej1.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import sys
 
-add_one_rep = lambda actual, single: 100 * (actual + (1 - actual) * single)
+add_one_rep = lambda x: x.ix[0] + (1 - x.ix[0]) * x.ix[1]
 
-if len(sys.argv != 3):
+if len(sys.argv) != 3:
     file = "tabla.csv"
     replicas = 3
 else:
@@ -11,5 +11,14 @@ else:
     replicas = int(sys.argv[2])
 
 # leemos un csv con la información de la disponibilidad de cada uno de los componentes
-availability_chart = pd.read_cs(file)
+availability_chart = pd.read_csv(file)
 
+# calculamos la disponibilidad al hacer una réplica de todos los servidores
+# availability_chart['Availability2'] = availability_chart[['Availability', 'Availability']].apply(add_one_rep, axis=1)
+
+for i in range(2,replicas+1):
+    name = 'Availability'+str(i)
+    last_name = 'Availability' if i==2 else 'Availability'+str(i-1)
+    availability_chart[name] = availability_chart[[last_name, 'Availability']].apply(add_one_rep, axis=1)
+
+print(availability_chart)
