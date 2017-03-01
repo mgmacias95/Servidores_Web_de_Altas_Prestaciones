70e0e2bf5973b756a26bacea6ba68729f42b4b76
mgmacias95 <mgmacias95@correo.ugr.es>
2017-03-01 23:06:48 +0100

    python script ended

--- teoria/tema2/ej1.py
+++ teoria/tema2/ej1.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import sys
+from tabulate import tabulate
 
 add_one_rep = lambda x: x.ix[0] + (1 - x.ix[0]) * x.ix[1]
 
@@ -13,12 +14,10 @@ else:
 # leemos un csv con la información de la disponibilidad de cada uno de los componentes
 availability_chart = pd.read_csv(file)
 
-# calculamos la disponibilidad al hacer una réplica de todos los servidores
-# availability_chart['Availability2'] = availability_chart[['Availability', 'Availability']].apply(add_one_rep, axis=1)
-
+# calculamos la disponibilidad las réplicas de todos los servidores
 for i in range(2,replicas+1):
     name = 'Availability'+str(i)
     last_name = 'Availability' if i==2 else 'Availability'+str(i-1)
     availability_chart[name] = availability_chart[[last_name, 'Availability']].apply(add_one_rep, axis=1)
 
-print(availability_chart)
+print(tabulate(availability_chart, headers='keys', tablefmt='pipe', showindex=False))
